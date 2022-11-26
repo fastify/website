08b014ed46aa0ad57781b43e50be9f5b5f35420b
Manuel Spigolon <behemoth89@gmail.com>
2022-11-26 11:06:36 +0100

    npx create docusaurus

--- docs/Guides/index.md
+++
@@ -1,8 +0,0 @@
----
-title: Guides Documentation
----
-
-## General
-<a id="guides-general"></a>
-
-* [Contributing To Fastify](./Contributing.md)
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,50 +1,53 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
-
-const BASE_URL = process.env.BASE_URL || "/";
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Fastify",
-  tagline: "Fast and low overhead web framework, for Node.js",
-  url: "https://fastify.io",
-  baseUrl: BASE_URL,
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "fastify", // Usually your GitHub org/user name.
-  projectName: "website-next", // Usually your repo name.
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
 
   presets: [
     [
-      "@docusaurus/preset-classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: "https://github.com/fastify/website-next/edit/main/docs/",
-          lastVersion: "current",
-          versions: {
-            current: {
-              label: "v3.x (Current)",
-              path: "v3",
-            },
-            v2: {
-              label: "v2.x",
-              path: "v2",
-            },
-            v1: {
-              label: "v1.x",
-              path: "v1",
-            },
-          },
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
@@ -54,91 +57,70 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
+        title: 'My Site',
         logo: {
-          alt: "Fastify",
-          src: "img/fastify-logo-black.png",
-          srcDark: "img/fastify-logo-white.png",
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
         },
         items: [
           {
-            type: "doc",
-            docId: "index",
-            position: "left",
-            label: "Docs",
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
           },
-          { to: "/ecosystem", label: "Ecosystem", position: "left" },
-          { to: "/benchmarks", label: "Benchmarks", position: "left" },
-          { to: "/contribute", label: "Contribute", position: "left" },
+          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: "https://medium.com/@fastifyjs",
-            label: "Blog",
-            position: "left",
-          },
-          {
-            href: "https://github.com/fastify/help",
-            label: "Help",
-            position: "left",
-          },
-          {
-            type: "docsVersionDropdown",
-            position: "right",
-          },
-          {
-            href: "https://github.com/fastify/fastify",
-            label: "GitHub",
-            position: "right",
-          },
-          {
-            href: "https://twitter.com/fastifyjs",
-            label: "Twitter",
-            position: "right",
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
           },
         ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
         links: [
           {
-            title: "Docs",
+            title: 'Docs',
             items: [
               {
-                label: "Docs",
-                to: "/docs/v3/",
+                label: 'Tutorial',
+                to: '/docs/intro',
               },
             ],
           },
           {
-            title: "Community",
+            title: 'Community',
             items: [
               {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/fastify",
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
               },
               {
-                label: "Discord",
-                href: "https://discord.gg/D3FZYPy",
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
               },
               {
-                label: "Twitter",
-                href: "https://twitter.com/fastifyjs",
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
               },
             ],
           },
           {
-            title: "More",
+            title: 'More',
             items: [
               {
-                label: "Blog",
-                href: "https://medium.com/@fastifyjs",
+                label: 'Blog',
+                to: '/blog',
               },
               {
-                label: "GitHub",
-                href: "https://github.com/fastify/fastify",
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} No One. This is a demo website.`,
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
