cd175691feb3908072f79f8b1024e10d86631d70
HassanBahati <mukisabahati@gmail.com>
2022-12-26 17:39:30 +0300

    add latest version to docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,51 +1,58 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-const BASE_URL = process.env.BASE_URL || '/';
+const BASE_URL = process.env.BASE_URL || "/";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Fastify',
-  tagline: 'Fast and low overhead web framework, for Node.js',
-  url: 'https://fastify.io',
+  title: "Fastify",
+  tagline: "Fast and low overhead web framework, for Node.js",
+  url: "https://fastify.io",
   baseUrl: BASE_URL,
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
-  organizationName: 'fastify', // Usually your GitHub org/user name.
-  projectName: 'website-next', // Usually your repo name.
+  organizationName: "fastify", // Usually your GitHub org/user name.
+  projectName: "website-next", // Usually your repo name.
 
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          showLastUpdateTime: true, 
+          lastVersion: "current",
+          versions: {
+            current: {
+              label: "v3.x (Current)",
+              path: "v3",
+            },
+          },
+          sidebarPath: require.resolve("./sidebars.js"),
+          showLastUpdateTime: true,
           includeCurrentVersion: true,
-          editUrl: 'https://github.com/fastify/website-next/edit/main/docs/'
+          editUrl: "https://github.com/fastify/website-next/edit/main/docs/",
         },
         blog: {
           showReadingTime: true,
-          editUrl: 'https://github.com/fastify/website-next/edit/main/docs/'
+          editUrl: "https://github.com/fastify/website-next/edit/main/docs/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
         sitemap: {
-          ignorePatterns: ['/scripts/**']
-        }
+          ignorePatterns: ["/scripts/**"],
+        },
       }),
     ],
   ],
@@ -54,11 +61,11 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Fastify',
+        title: "Fastify",
         logo: {
-          alt: 'Fastify Cheetah Logo',
-          src: 'img/logos/fastify-black.png',
-          srcDark: 'img/logos/fastify-white.png',
+          alt: "Fastify Cheetah Logo",
+          src: "img/logos/fastify-black.png",
+          srcDark: "img/logos/fastify-white.png",
         },
         items: [
           // {
@@ -67,7 +74,7 @@ const config = {
           //   position: 'left',
           //   label: 'Tutorial',
           // },
-          { to: '/blog', label: 'Blog', position: 'left' },
+          { to: "/blog", label: "Blog", position: "left" },
           {
             type: "docsVersionDropdown",
             position: "right",
@@ -77,52 +84,52 @@ const config = {
           //   position: 'right',
           // },
           {
-            href: 'https://github.com/fastify/website-next',
-            position: 'right',
-            className: 'header-github-link',
-            'aria-label': 'GitHub repository',
+            href: "https://github.com/fastify/website-next",
+            position: "right",
+            className: "header-github-link",
+            "aria-label": "GitHub repository",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "Tutorial",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/fastify',
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/fastify",
               },
               {
-                label: 'Discord',
-                href: 'https://discord.gg/fastify',
+                label: "Discord",
+                href: "https://discord.gg/fastify",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/fastifyjs',
+                label: "Twitter",
+                href: "https://twitter.com/fastifyjs",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Blog",
+                to: "/blog",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/fastify/website-next',
+                label: "GitHub",
+                href: "https://github.com/fastify/website-next",
               },
             ],
           },
@@ -134,20 +141,20 @@ const config = {
         darkTheme: darkCodeTheme,
         magicComments: [
           {
-            className: 'theme-code-block-highlighted-line',
-            line: 'highlight-next-line',
-            block: { start: 'highlight-start', end: 'highlight-end' },
+            className: "theme-code-block-highlighted-line",
+            line: "highlight-next-line",
+            block: { start: "highlight-start", end: "highlight-end" },
           },
           {
-            className: 'code-block-error-line',
-            line: 'This will error',
+            className: "code-block-error-line",
+            line: "This will error",
           },
         ],
       },
       algolia: {
-        appId: 'DMPMC33PLU',
-        apiKey: '12d46b3bfeee6511031cfe00778f3e45',
-        indexName: 'fastify',
+        appId: "DMPMC33PLU",
+        apiKey: "12d46b3bfeee6511031cfe00778f3e45",
+        indexName: "fastify",
       },
     }),
 };
