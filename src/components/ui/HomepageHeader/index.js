eb5148ac8ec0eb5651726eaab98e3de455c58c9e
HassanBahati <mukisabahati@gmail.com>
2022-12-27 14:07:17 +0300

    move HomePageHeader into ui/components

---
+++ src/components/ui/HomepageHeader/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "@site/src/pages/index.module.css";
+
+const HomepageHeader = () => {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default HomepageHeader;
