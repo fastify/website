780dacb8a3f014785a1a88f7a55407a871b43a02
HassanBahati <mukisabahati@gmail.com>
2022-12-28 16:07:42 +0300

    add github buttons to header

--- src/components/ui/HomepageHeader/index.tsx
+++ src/components/ui/HomepageHeader/index.tsx
@@ -3,6 +3,7 @@ import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
+import GitHubButton from "react-github-btn";
 
 const HomepageHeader = () => {
   const { siteConfig } = useDocusaurusContext();
@@ -10,22 +11,38 @@ const HomepageHeader = () => {
     // <header className={clsx("hero hero--primary", styles.heroBanner)}>
     <header className={styles.hero} data-theme="light">
       <div className="container">
-        <img
-          alt={siteConfig.title}
-          className={styles.heroLogo}
-          src={`img/logos/fastify-white.png`}
-        />
-        <p className={styles.heroProjectTagline}>{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
+        <h1 className="title">
+          <img
+            alt={siteConfig.title}
+            className={styles.heroLogo}
+            src={`img/logos/fastify-white.png`}
+          />
+        </h1>
+
+        <h2 className={styles.heroProjectTagline}>{siteConfig.tagline}</h2>
+        <div className="github-buttons">
+          <GitHubButton
+            href="https://github.com/fastify/fastify"
+            data-icon="octicon-star"
+            aria-label="Star fastify/fastify on GitHub"
+            data-size="large"
+            data-show-count="true"
           >
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+            Star
+          </GitHubButton>
+          <GitHubButton
+            href="https://github.com/fastify/fastify/fork"
+            data-icon="octicon-repo-forked"
+            aria-label="Fork fastify/fastify on GitHub"
+            data-size="large"
+          >
+            Fork
+          </GitHubButton>
         </div>
       </div>
-      
+      <div
+      // className="hero"
+      ></div>
     </header>
   );
 };
