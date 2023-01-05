5b787fdc849ea8cca606a960f3445c5191992d72
HassanBahati <mukisabahati@gmail.com>
2023-01-05 12:28:52 +0300

    fix: typo

--- src/components/CustomHighLight/index.js
+++ src/components/CustomHighLight/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./index.module.css";
 
-const CustomHighLight = ({text:Str}) => {
+const CustomHighLight = ({text}) => {
   return <code className={styles.textHighlight}>{text}</code>;
 };
 
