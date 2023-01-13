63f12a284047ddcecef5784166c2b22ebb1fba50
Manuel Spigolon <behemoth89@gmail.com>
2023-01-13 18:44:55 +0100

    chore: removed unused css

--- src/components/BenchmarkInfo/index.jsx
+++ src/components/BenchmarkInfo/index.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
+import styles from './styles.module.css'
 
 import Link from '@docusaurus/Link'
 
 function BenchmarkInfo({ name, test: testFile, repository, req, progressValue }) {
   return (
-    <div className="box shadow--md">
-      <div className="level">
-        <div className="level-left">
+    <div className={`${styles.box} shadow--md`}>
+      <div className={styles.level}>
+        <div>
           <Link to={testFile}>
             <h2>{name}</h2>
           </Link>
         </div>
-        <div className="level-right">
+        <div>
           <Link className="header-github-link" to={repository} />
         </div>
       </div>
       <p>{Math.round(req) + ' req/sec'}</p>
-      <progress key="component" className="progress is-primary" value={progressValue} max="1">
+      <progress className={`${styles.progress} ${specialColor(name)}`} value={progressValue} max="1">
         {progressValue * 100 + '%'}
       </progress>
     </div>
   )
+}
+
+function specialColor(name) {
+  return name === 'Fastify' ? styles.progressFastify : styles.progressOthers
 }
 
 export default BenchmarkInfo
