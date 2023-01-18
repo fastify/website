22c0c818635f706ae576547743c3cc8498aa39fe
Manuel Spigolon <behemoth89@gmail.com>
2023-01-18 20:01:39 +0100

    fix: styling

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext()
 
   return (
-    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+    <Layout title={siteConfig.tagline} description={siteConfig.tagline}>
       <HomepageHeader />
       <main>
         <section className="section">
