b4fab02bbfc2e44c1160289a15b6b0facf2940cf
HassanBahati <mukisabahati@gmail.com>
2023-01-18 14:51:32 +0300

    feat: add home page header

---
+++ src/components/HomePageHeader/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import GitHubButton from 'react-github-btn'
+
+import css from './index.module.css'
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext()
+
+  return (
+    <header className={css.hero}>
+      <img alt={siteConfig.title} src={`img/logos/fastify-white.png`} />
+
+      <h2>{siteConfig.tagline}</h2>
+
+      <div>
+        <GitHubButton
+          href="https://github.com/fastify/fastify"
+          data-icon="octicon-star"
+          aria-label="Star fastify on GitHub"
+          data-size="large"
+          data-show-count="true">
+          Star
+        </GitHubButton>
+        &nbsp; &nbsp;
+        <GitHubButton
+          href="https://github.com/fastify/fastify/fork"
+          data-icon="octicon-repo-forked"
+          aria-label="Fork fastify on GitHub"
+          data-size="large">
+          Fork
+        </GitHubButton>
+      </div>
+    </header>
+  )
+}
+
+export default HomepageHeader
