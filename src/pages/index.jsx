ca44ca92a9dd75d251be9332b58f17c120159d50
HassanBahati <mukisabahati@gmail.com>
2023-01-18 15:06:23 +0300

    feat: add why fastify section to home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -30,7 +30,21 @@ export default function Home() {
     <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <section className="section">
+          <div className="container content">
+            <h1>Why</h1>
+            <p>
+              An efficient server implies a lower cost of the infrastructure, a better responsiveness under load and
+              happy users. How can you efficiently handle the resources of your server, knowing that you are serving the
+              highest number of requests possible, without sacrificing security validations and handy development?
+            </p>
+            <p>
+              Enter Fastify. Fastify is a web framework highly focused on providing the best developer experience with
+              the least overhead and a powerful plugin architecture. It is inspired by Hapi and Express and as far as we
+              know, it is one of the fastest web frameworks in town.
+            </p>
+          </div>
+        </section>
       </main>
     </Layout>
   )
