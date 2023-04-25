117b91ecad646c2301e471db82ebc1f98ad69443
Manuel Spigolon <manuel.spigolon@nearform.com>
2023-04-25 12:10:39 +0200

    add: index section ts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -84,42 +84,7 @@ export default function Home() {
 
         <section className="section">
           <div className="container">
-            <QuickStart
-              esm={`// Import the framework and instantiate it
-import Fastify from 'fastify'
-const fastify = Fastify({
-  logger: true
-})
-
-// Declare a route
-fastify.get('/', async function handler (request, reply) {
-  return { hello: 'world' }
-})
-
-// Run the server!
-try {
-  await fastify.listen({ port: 3000 })
-} catch (err) {
-  fastify.log.error(err)
-  process.exit(1)
-}
-            `}
-              cjs={`// Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true })
-
-// Declare a route
-fastify.get('/', function handler (request, reply) {
-  reply.send({ hello: 'world' })
-})
-
-// Run the server!
-fastify.listen({ port: 3000 }, (err) => {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-})`}
-            />
+            <QuickStart />
           </div>
         </section>
 
