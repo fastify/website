535a62ab0a9f1de152bcfcd7bf49e05b8ed3535c
Manuel Spigolon <manuel.spigolon@nearform.com>
2024-05-12 15:55:53 +0200

    lint

--- src/components/Organisations/index.jsx
+++ src/components/Organisations/index.jsx
@@ -22,17 +22,13 @@ export default function Organisations({ maxItems, onlySponsors }) {
 
 function OrganizationItem({ organization }) {
   return (
-    <a href={organization.link} target="_blank" rel="noreferrer"
-      className={getOrganizationStyle(organization)}>
-      <img
-        src={useBaseUrl(`/img/organisations/${organization.image}`)}
-        alt={`${organization.name} is using Fastify`}
-      />
+    <a href={organization.link} target="_blank" rel="noreferrer" className={getOrganizationStyle(organization)}>
+      <img src={useBaseUrl(`/img/organisations/${organization.image}`)} alt={`${organization.name} is using Fastify`} />
     </a>
   )
 }
 
-function getOrganizationStyle (organization) {
+function getOrganizationStyle(organization) {
   if (organization.sponsor) {
     return styles[`sponsoring_${organization.tier}`]
   }
