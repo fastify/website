0584e334630a5a1ab83b9fa79dba7a34672b4b2b
Abhiraj Marne <111980225+Abmarne@users.noreply.github.com>
2026-01-18 16:22:26 +0530

    fix: social images (#378)
    * Fixes #377
    
    * Lint
    
    * Updated url
    
    * Updated url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -207,6 +207,32 @@ const config = {
           },
         ],
       },
+
+      metadata: [
+        // Open Graph / Facebook
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://fastify.dev/' },
+        { property: 'og:title', content: 'Fastify - Fast and Low Overhead Web Framework' },
+        { property: 'og:description', content: 'Fastify is a fast and low overhead web framework for Node.js.' },
+        { property: 'og:image', content: 'https://fastify.dev/img/logos/fastify-black.svg' },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '630' },
+        { property: 'og:image:alt', content: 'Fastify Logo' },
+        { property: 'og:site_name', content: 'Fastify' },
+        { property: 'og:locale', content: 'en_US' },
+
+        // Twitter
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:url', content: 'https://fastify.dev/' },
+        { name: 'twitter:title', content: 'Fastify - Fast and Low Overhead Web Framework' },
+        { name: 'twitter:description', content: 'Fastify is a fast and low overhead web framework for Node.js.' },
+        { name: 'twitter:image', content: 'https://fastify.dev/img/logos/fastify-black.svg' },
+
+        // Telegram (non-standard but harmless)
+        { property: 'telegram:title', content: 'Fastify - Fast and Low Overhead Web Framework' },
+        { property: 'telegram:description', content: 'Fastify is a fast and low overhead web framework for Node.js.' },
+        { property: 'telegram:image', content: 'https://fastify.dev/img/logos/fastify-black.svg' },
+      ],
     }),
 
   plugins: [
